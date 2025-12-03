c1ec5efcccfc1faa43ec54200c50067248d1eb71
Edvaldo <edvaldovitor250@gmail.com>
2025-12-02 21:27:13 -0300

    feat: Implement Fastpay payment integration
    - Added FastpayPaymentAPIClient interface for payment operations.
    - Created FastpayPaymentAPIClientConfig for client configuration.
    - Introduced FastpayPaymentInput class for payment request data.
    - Defined FastpayPaymentMethod enum for payment methods.
    - Developed FastpayPaymentModel class to represent payment responses.
    - Established FastpayPaymentStatus enum for tracking payment statuses.

---
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/FastpayPaymentModel.java
@@ -0,0 +1,14 @@
+package com.algaworks.algashop.billing.infrastructure.payment.fastpay;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class FastpayPaymentModel {
+    private String id;
+    private String referenceCode;
+    private String status;
+    private BigDecimal totalAmount;
+
+}
