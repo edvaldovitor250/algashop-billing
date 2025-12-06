8d17a2939fc9169bbc55012013c22114678b8353
Edvaldo <edvaldovitor250@gmail.com>
2025-12-06 09:59:41 -0300

    feat: Implement Credit Card management and query services with necessary DTOs

--- src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
@@ -4,5 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
+import jdk.jfr.internal.Options;
+import java.util.List;
+
+
 public interface CreditCardRepository  extends JpaRepository<CreditCard, UUID> {
+
+    Opitinal<CreditCard> findByCustomerIdAndId(UUID customerId);
 }
